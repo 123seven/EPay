350e832bf2d0cb85be35940532cb3275385923db
seven <seven@seven123.top>
2023-05-22 00:02:09 +0800

    🦄 refactor: 登录框

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,3 @@
-
 const Logo = () => (
   <span className="w-5 h-5 mr-2 text-black">
     <svg
@@ -34,16 +33,71 @@ export default function SignIn() {
           <div className="w-full mx-auto mt-8">
             <div className="bg-white ">
               <div className="flex flex-col gap-3 mt-6">
-                <label className="text-sm font-medium text-gray-700">
-                  signInEmail
-                </label>
-                <input className="input" type="text" />
+                <div className="relative flex items-center mt-8">
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-3 text-gray-300"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                      />
+                    </svg>
+                  </span>
+
+                  <input
+                    v-model="SignInData.account"
+                    type="account"
+                    className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-10  focus:border-blue-400 d focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="账户名"
+                  />
+                </div>
+
+                <div className="relative flex items-center mt-4">
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-3 text-gray-300"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                      />
+                    </svg>
+                  </span>
+
+                  <input
+                    v-model="SignInData.password"
+                    type="password"
+                    className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg  focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="账户密码"
+                  />
+                </div>
+
+                <div className="mt-6">
+                  <button
+                    // click="SignIn"
+                    className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                  >
+                    登录
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="px-8 py-6 text-sm font-normal text-gray-500 flex-initial">
-          by Epay
           <Logo></Logo>
         </div>
       </div>
